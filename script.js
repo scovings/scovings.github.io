b59c3a90cbbcea0afbdeee81fe0dce8013949efb
sco. <77859647+scovings@users.noreply.github.com>
2025-03-04 21:43:31 +0000

    siiick

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const introText = "Welcome to My Projects";
+  let index = 0;
+  const introElement = document.getElementById("intro");
+
+  function typeEffect() {
+      if (index < introText.length) {
+          introElement.innerHTML += introText.charAt(index);
+          index++;
+          setTimeout(typeEffect, 100);
+      } else {
+          setTimeout(() => {
+              introElement.classList.add("fade-out");
+          }, 1000);
+      }
+  }
+
+  introElement.innerHTML = ""; 
+  typeEffect();
+});
